1571348598a65042509fd8e7d3f5e8c8e77300f3
XantNero <dima.kukla.2001@gmail.com>
2021-04-22 22:34:56 +0300

    update error handling

--- src/main/java/com/web/restservice/GlobalExceptionHandlingControllerAdvice.java
+++ src/main/java/com/web/restservice/GlobalExceptionHandlingControllerAdvice.java
@@ -1,44 +1,44 @@
 package com.web.restservice;
 
-import org.springframework.web.client.HttpClientErrorException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.client.HttpServerErrorException;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import org.springframework.http.converter.HttpMessageNotReadableException;
+
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Date;
 
 @ControllerAdvice
-public class GlobalExceptionHandlingControllerAdvice /*extends ResponseEntityExceptionHandler*/{
+public class GlobalExceptionHandlingControllerAdvice{
 
-	// public GlobalExceptionHandlingControllerAdvice() {
-    //     super();
-    // }
+	@ExceptionHandler(ResponseStatusException.class)
+	@ResponseBody
+	public ResponseEntity<ExceptionResponse> handleResposeStatusException(ResponseStatusException ex, WebRequest request) {
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getReason(),
+        request.getDescription(false),ex.getStatus().getReasonPhrase());
+		return 	new ResponseEntity<ExceptionResponse>(exceptionResponse, ex.getStatus());
+	}
 
-	@ExceptionHandler(HttpClientErrorException.class)
-	public ResponseEntity<ExceptionResponse> badRequest(HttpClientErrorException ex, WebRequest request) {
-		
+
+	@ExceptionHandler(NumberFormatException.class)
+	@ResponseBody
+	public ResponseEntity<ExceptionResponse> handleNumberFormatException(NumberFormatException ex, WebRequest request) {
 		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(),
         request.getDescription(false),HttpStatus.BAD_REQUEST.getReasonPhrase());
 		return  new ResponseEntity<ExceptionResponse>(exceptionResponse, HttpStatus.BAD_REQUEST);
 	}
 
-	@ExceptionHandler(HttpServerErrorException.class)
-	public ResponseEntity<ExceptionResponse> internalError(HttpServerErrorException ex, WebRequest request) {
-		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(),
-        request.getDescription(false),HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
-		return  new ResponseEntity<ExceptionResponse>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
-	}
-
-	@ExceptionHandler(NumberFormatException.class)
-	public ResponseEntity<ExceptionResponse> internalError(NumberFormatException ex, WebRequest request) {
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	@ResponseBody
+	public ResponseEntity<ExceptionResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex, WebRequest request) {
 		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(),
         request.getDescription(false),HttpStatus.BAD_REQUEST.getReasonPhrase());
 		return  new ResponseEntity<ExceptionResponse>(exceptionResponse, HttpStatus.BAD_REQUEST);
